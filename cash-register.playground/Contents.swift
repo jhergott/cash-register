0efc3955e37b05d9d8ba9da4bac94914595af480
Jake Hergott <jake@rocketmiles.com>
2017-01-24 10:49:59 -0600

    more comments

--- cash-register.playground/Contents.swift
+++ cash-register.playground/Contents.swift
@@ -6,6 +6,8 @@
  we get too much and sometimes we get too little. Create a cash register that should be able to accept only $20, $10, $5, $2 and $1
  bills. Given the charge and amount of money received return the change in each denomination that should be given from the cash register. 
  Sometimes when the vendors couldn’t make exact change they would tell us they couldn’t make exact change.
+ 
+ Note: This solution is scoped to the crux of this problem which is the algorithm for making change.
  */
 
 import Foundation
